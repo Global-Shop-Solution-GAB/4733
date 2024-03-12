a1f38e3a2dd3b26586020d80e46b1bc34a29b521
Service Web <sw@gssmail.com>
2024-03-12 12:36:40 -0500

    Commit changes in C:\Websites\Git\emarlow\4733\GAB_4733_TIME.sql

---
+++ GAB_4733_TIME.sql
@@ -0,0 +1,19 @@
+/* ==========================================
+ * TABLE: GAB_4733_TIME
+ * ========================================== */
+CREATE TABLE "GAB_4733_TIME"(
+ "WEEK" CHAR(50) NOT NULL ,
+ "DATE" DATE NOT NULL ,
+ "DAY" CHAR(10) NOT NULL ,
+ "CUSTOMER" CHAR(30),
+ "DAYS" DOUBLE,
+ "HOURS_VT" DOUBLE,
+ "HOURS_RD" DOUBLE,
+ "HOURS_SALES" DOUBLE,
+ "HOURS_NB" DOUBLE,
+ "RATE" DOUBLE,
+ "EXTENSION" DOUBLE,
+ "NOTES" LONGVARCHAR,
+ "ID" INTEGER NOT NULL );
+
+
